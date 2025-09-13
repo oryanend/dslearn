c46c6280a94b78d585ee52a92a490c6aafa91de8
oryanend <ryanyamim@gmail.com>
2025-09-13 19:02:53 -0300

    Dslearn

---
+++ src/main/java/com/oryanend/dslern/DslernApplication.java
@@ -0,0 +1,13 @@
+package com.oryanend.dslern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslernApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslernApplication.class, args);
+    }
+
+}
