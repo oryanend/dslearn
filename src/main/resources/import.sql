dec4f237d0bd3f8a223e90b56169100c07ad883c
oryanend <ryanyamim@gmail.com>
2025-09-13 19:45:05 -0300

    Entity Course Added

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ( 'Bootcamp HTML', 'https://cdn.mos.cms.futurecdn.net/NWJ4mV8N7BGMLhyEAsb85U.jpg', 'https://happycoding.io/tutorials/html/images/html-3.png');
